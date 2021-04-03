376e779ee0db5235b8e9d1d3c285390e279de49e
sakiyamaK <sakiyama.k@gmail.com>
2021-04-03 19:49:19 +0900

    rikai

--- SampleCompositonalLayout/CompositionalLayout/CompositionalLayout08/ViewController/CompositionalLayout08ViewController.swift
+++ SampleCompositonalLayout/CompositionalLayout/CompositionalLayout08/ViewController/CompositionalLayout08ViewController.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 final class CompositionalLayout08ViewController: UIViewController {
-
   private var items: [SampleImageModel] = []
+
+  @IBOutlet var segmentControll: UISegmentedControl! {
+    didSet {
+      segmentControll.addTarget(self, action: #selector(changeSegment), for: .valueChanged)
+    }
+  }
 
   @IBOutlet private var collectionView: UICollectionView! {
     didSet {
       collectionView.collectionViewLayout = layout
-      collectionView.register(UINib.init(nibName: UIImageViewCell.reuseId, bundle: nil), forCellWithReuseIdentifier: UIImageViewCell.reuseId)
+      collectionView.register(UINib(nibName: UIImageViewCell.reuseId, bundle: nil), forCellWithReuseIdentifier: UIImageViewCell.reuseId)
       collectionView.backgroundColor = .systemGray
     }
   }
@@ -22,14 +27,27 @@ final class CompositionalLayout08ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    SampleImageModel.load(times: 50) {[unowned self] items in
+    SampleImageModel.load(times: 50) { [unowned self] items in
       self.items = items
       self.collectionView.reloadData()
     }
   }
+
+  @objc func changeSegment() {
+    updateLayout()
+  }
 }
 
 private extension CompositionalLayout08ViewController {
+  func updateLayout() {
+    collectionView.collectionViewLayout = layout
+    collectionView.alpha = 0.0
+    collectionView.reloadData()
+    UIView.animate(withDuration: 0.25, animations: {
+      self.collectionView.alpha = 1.0
+    })
+  }
+
   var layout: UICollectionViewLayout {
     UICollectionViewCompositionalLayout { [unowned self] (section, environment) -> NSCollectionLayoutSection? in
 
@@ -38,30 +56,31 @@ private extension CompositionalLayout08ViewController {
         heightDimension: .estimated(environment.container.effectiveContentSize.height)
       )
 
+      let groupInsets = NSDirectionalEdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8)
       let group = NSCollectionLayoutGroup.custom(layoutSize: groupSize) { [unowned self] (environment) -> [NSCollectionLayoutGroupCustomItem] in
 
         var items: [NSCollectionLayoutGroupCustomItem] = []
 
         var layouts: [Int: CGFloat] = [:]
 
-        let space: CGFloat = 8
-        let numberOfColumn: CGFloat = 2
+        let horizontalSpace: CGFloat = 8
+        let verticalSpace: CGFloat = 8
+        let numberOfColumn: CGFloat = self.segmentControll.selectedSegmentIndex == 0 ? 1.0 : 2.0
 
         let defaultSize = CGSize(width: 100, height: 100)
 
-        for idx in (0 ..< self.collectionView.numberOfItems(inSection: section)) {
-
+        for idx in 0 ..< self.collectionView.numberOfItems(inSection: section) {
           let size = self.items[idx].image?.size ?? defaultSize
           let aspect = CGFloat(size.height) / CGFloat(size.width)
 
-          let width = (environment.container.effectiveContentSize.width - (numberOfColumn - 1) * space) / numberOfColumn
+          let width = (environment.container.effectiveContentSize.width - (numberOfColumn - 1) * horizontalSpace) / numberOfColumn - (groupInsets.leading + groupInsets.trailing)
           let height = width * aspect
 
           let currentColumn = idx % Int(numberOfColumn)
-          let y = layouts[currentColumn] ?? 0.0 + space
-          let x = width * CGFloat(currentColumn) + space * (CGFloat(currentColumn) - 1.0)
+          let y = layouts[currentColumn] ?? 0.0 + verticalSpace
+          let x = width * CGFloat(currentColumn) + horizontalSpace * (CGFloat(currentColumn) + 1) + groupInsets.leading
 
-          let frame = CGRect(x: x, y: y + space, width: width, height: height)
+          let frame = CGRect(x: x, y: y + verticalSpace, width: width, height: height)
           let item = NSCollectionLayoutGroupCustomItem(frame: frame)
           items.append(item)
 
@@ -69,6 +88,8 @@ private extension CompositionalLayout08ViewController {
         }
         return items
       }
+      group.contentInsets = groupInsets
+
       return NSCollectionLayoutSection(group: group)
     }
   }
